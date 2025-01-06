7199dd799f6f160f8e755a12aa09a572c1260c0b
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2025-01-06 20:27:26 +0530

    Create UserServlet.java

---
+++ UserServlet.java
@@ -0,0 +1,28 @@
+package com.example.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to the User Management System</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+
+        request.setAttribute("username", username);
+        request.setAttribute("email", email);
+
+        request.getRequestDispatcher("/WEB-INF/views/userProfile.jsp").forward(request, response);
+    }
+}
